03960bcbb66494d6873c93cef948aeecdc9a0937
Gabor Molnar <gabormolnar92@gmail.com>
2014-11-13 14:17:15 +0100

    Hozzászólások javítás

--- app/views/site/event/show.blade.php
+++ app/views/site/event/show.blade.php
@@ -27,7 +27,18 @@
     @endif
 
     <h4>Hozzászólások</h4>
+    <div id="disqus_thread"></div>
+    <script type="text/javascript">
+        /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+        var disqus_shortname = 'szentmiklosovi'; // required: replace example with your forum shortname
 
-    <div class="fb-comments" data-href="{{$url}}" data-width="100%" data-numposts="10" data-colorscheme="light"></div>
+        /* * * DON'T EDIT BELOW THIS LINE * * */
+        (function() {
+            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        })();
+    </script>
+
 </div>
 @stop
