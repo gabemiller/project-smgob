092777a1b6569fa584e8685c3ba8fd6f1484902a
Gabor Molnar <gabormolnar92@gmail.com>
2014-11-28 22:48:27 +0100

    Gallery bug fix

--- app/views/site/gallery/index.blade.php
+++ app/views/site/gallery/index.blade.php
@@ -11,16 +11,18 @@
     <div class="gallery">
         <ul class="row list-unstyled">
             @foreach($galleries as $gallery)
-                <li class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
-                    <div class="gallery-item">
-                        {{HTML::decode(HTML::linkRoute('galeriak.show','<img class="img-responsive" src="'.$gallery->pictures->first()->thumbnail_path.'" alt="'.$gallery->name.'" title="'.$gallery->name.'" />',array('id'=>$gallery->id,'title'=>$gallery->getSlugName())))}}
-                        {{HTML::decode(HTML::linkRoute('galeriak.show','<h3>'.$gallery->name.'</h3>',array('id'=>$gallery->id,'title'=>$gallery->getSlugName())))}}
-                        @if(strlen($gallery->getDescription())>0)
-                            <p>{{$gallery->getDescription()}}</p>
-                        @endif
-                        <p class="small text-muted gallery-update">Frissítve: {{$gallery->getUpdateDate()}}</p>
-                    </div>
-                </li>
+                @if(count($gallery->pictures)>0)
+                    <li class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+                        <div class="gallery-item">
+                            {{HTML::decode(HTML::linkRoute('galeriak.show','<img class="img-responsive" src="'.$gallery->pictures->first()->thumbnail_path.'" alt="'.$gallery->name.'" title="'.$gallery->name.'" />',array('id'=>$gallery->id,'title'=>$gallery->getSlugName())))}}
+                            {{HTML::decode(HTML::linkRoute('galeriak.show','<h3>'.$gallery->name.'</h3>',array('id'=>$gallery->id,'title'=>$gallery->getSlugName())))}}
+                            @if(strlen($gallery->getDescription())>0)
+                                <p>{{$gallery->getDescription()}}</p>
+                            @endif
+                            <p class="small text-muted gallery-update">Frissítve: {{$gallery->getUpdateDate()}}</p>
+                        </div>
+                    </li>
+                @endif
             @endforeach
         </ul>
 
