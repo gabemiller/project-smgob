6dc6fd451b1b00aae158a7018cf6e2b054e86425
Gabor Molnar <gabormolnar92@gmail.com>
2014-11-28 22:24:01 +0100

    Design
    article
    event
    gallery

--- app/controllers/Site/ArticleController.php
+++ app/controllers/Site/ArticleController.php
@@ -23,8 +23,6 @@ class ArticleController extends \BaseController {
         $article = Article::find($id);
         
         View::share('title', $article->title);
-
-
         
         $this->layout->content = View::make('site.article.show')->with('article', $article)->with('url',Request::url());
     }
--- app/views/site/gallery/index.blade.php
+++ app/views/site/gallery/index.blade.php
@@ -1,26 +1,31 @@
 @extend('_frontend.master')
+
+@section('page-title')
+    <h1>Galériák</h1>
+@stop
+
 @section('breadcrumb')
-{{ HTML::decode(Breadcrumbs::render('galeriak.index')) }}
+    {{ HTML::decode(Breadcrumbs::render('galeriak.index')) }}
 @stop
 @section('content')
-<div class="gallery">
-    <ul class="row list-unstyled">
-        @foreach($galleries as $gallery)
-        <li class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
-            <div class="gallery-item">
-                {{HTML::decode(HTML::linkRoute('galeriak.show','<img class="img-responsive" src="'.$gallery->pictures->first()->thumbnail_path.'" alt="'.$gallery->name.'" title="'.$gallery->name.'" />',array('id'=>$gallery->id,'title'=>$gallery->getSlugName())))}}
-                {{HTML::decode(HTML::linkRoute('galeriak.show','<h3>'.$gallery->name.'</h3>',array('id'=>$gallery->id,'title'=>$gallery->getSlugName())))}}
-                @if(strlen($gallery->getDescription())>0)
-                <p>{{$gallery->getDescription()}}</p>
-                @endif
-                <p class="small text-muted gallery-update">Frissítve: {{$gallery->getUpdateDate()}}</p>
-            </div>
-        </li>
-        @endforeach
-    </ul>
+    <div class="gallery">
+        <ul class="row list-unstyled">
+            @foreach($galleries as $gallery)
+                <li class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+                    <div class="gallery-item">
+                        {{HTML::decode(HTML::linkRoute('galeriak.show','<img class="img-responsive" src="'.$gallery->pictures->first()->thumbnail_path.'" alt="'.$gallery->name.'" title="'.$gallery->name.'" />',array('id'=>$gallery->id,'title'=>$gallery->getSlugName())))}}
+                        {{HTML::decode(HTML::linkRoute('galeriak.show','<h3>'.$gallery->name.'</h3>',array('id'=>$gallery->id,'title'=>$gallery->getSlugName())))}}
+                        @if(strlen($gallery->getDescription())>0)
+                            <p>{{$gallery->getDescription()}}</p>
+                        @endif
+                        <p class="small text-muted gallery-update">Frissítve: {{$gallery->getUpdateDate()}}</p>
+                    </div>
+                </li>
+            @endforeach
+        </ul>
 
-    <div class="text-center">
-        {{$galleries->links();}}
+        <div class="text-center">
+            {{$galleries->links();}}
+        </div>
     </div>
-</div>
 @stop
